0b1f838435cb1c3e38216cde60736f48c816d533
ChoRongChoRong <46623282+ChoRongChoRong-Saseul@users.noreply.github.com>
2023-09-27 23:31:39 +0900

    Add files via upload

--- saseul_start.sh
+++ saseul_start.sh
@@ -3,13 +3,13 @@
 clear
 
 
-END=85
+END=84
 
 PeerNodeAddress='main.saseul.net'
 
 
 PS3='메뉴를 선택해주세요. : '
-foods=("1.Start" "2.StartMining" "3.FroceSync" "4.Exit")
+foods=("1.Start" "2.StartMining" "3.FroceSync" "4.SingeFroceSync" "5.Log" "6.Exit")
 
 select fav in "${foods[@]}"; do
     case $fav in
@@ -20,12 +20,14 @@ select fav in "${foods[@]}"; do
 			while [ $number -le $END ]
 			do
 				A=$(docker exec -i saseul-node$number saseul-script start)
-				echo "$number: "$A
-				sleep 1
+				echo "**********"
+				echo "$number: ""$A"
+				echo "**********"
 				((number++))
 			done
-			echo " "
+			echo "*********************************************************"
 			echo "${foods[@]}"
+			echo "*********************************************************"
             ;;
         "2.StartMining")
             echo "Starting mining ..."
@@ -34,12 +36,14 @@ select fav in "${foods[@]}"; do
 			while [ $number -le $END ]
 			do
 				A=$(docker exec -i saseul-node$number saseul-script startmining)
-				echo "$number: "$A
-				sleep 1
+				echo "**********"
+				echo "$number: ""$A"
+				echo "**********"
 				((number++))
 			done
-			echo " "
+			echo "*********************************************************"
 			echo "${foods[@]}"
+			echo "*********************************************************"
             ;;
         "3.FroceSync")
             echo "FroceSync ..."
@@ -48,14 +52,42 @@ select fav in "${foods[@]}"; do
 			while [ $number -le $END ]
 			do
 				A=$(docker exec -i saseul-node$number saseul-script forcesync -p $PeerNodeAddress)
-				echo "$number: "$A
-				sleep 5
+				echo "**********"
+				echo "$number: ""$A"
+				echo "**********"
 				((number++))
 			done
-			echo " "
+			echo "*********************************************************"
 			echo "${foods[@]}"
+			echo "*********************************************************"
             ;;
-	"4.Exit")
+        "4.SingeFroceSync")
+            echo "싱크하실 노드번호를 입혁하세요."
+			read SINGLE
+			A=$(docker exec -i saseul-node$SINGLE saseul-script forcesync -p $PeerNodeAddress)
+			echo "$A"
+			echo " "
+			echo "*********************************************************"
+			echo "${foods[@]}"
+			echo "*********************************************************"
+            ;;
+        "5.Log")
+            echo "Log ..."
+			
+			number=80
+			while [ $number -le $END ]
+			do
+				A=$(docker exec -i saseul-node$number saseul-script log )
+				echo "**********"
+				echo "Node $number Port: ""$A"
+				echo "**********"
+				((number++))
+			done
+			echo "*********************************************************"
+			echo "${foods[@]}"
+			echo "*********************************************************"
+            ;;
+	"6.Exit")
 	    echo "작업을 종료했습니다."
 	    exit
 	    ;;
