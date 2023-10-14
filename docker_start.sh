f841323ef3b62549a2c585c7c6cc2d745939c56d
ChoRongChoRong <46623282+ChoRongChoRong-Saseul@users.noreply.github.com>
2023-10-14 19:38:38 +0900

    Create docker_start.sh

---
+++ docker_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash --login
+
+docker start saseul-node80
+docker start saseul-node81
+docker start saseul-node82
+docker start saseul-node83
+docker start saseul-node84
+docker start saseul-node85
+docker start saseul-node86
+docker start saseul-node88
+docker start saseul-node90
