d2c2b4f8e355cd1f8a8980027c4584353d89e6f0
ChoRongChoRong <46623282+ChoRongChoRong-Saseul@users.noreply.github.com>
2023-10-14 19:39:08 +0900

    Create docker_stop.sh

---
+++ docker_stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker stop saseul-node80
+docker stop saseul-node81
+docker stop saseul-node82
+docker stop saseul-node83
+docker stop saseul-node84
+docker stop saseul-node85
+docker stop saseul-node86
+docker stop saseul-node88
+docker stop saseul-node89
+docker stop saseul-node90
