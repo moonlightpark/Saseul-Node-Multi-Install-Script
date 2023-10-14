38603dd5b9bed1e4b43a5b6a2511639d6efd7090
ChoRongChoRong <46623282+ChoRongChoRong-Saseul@users.noreply.github.com>
2023-10-14 19:38:05 +0900

    Create docker_restart.sh

---
+++ docker_restart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash --login
+
+docker restart saseul-node80
+docker restart saseul-node81
+docker restart saseul-node82
+docker restart saseul-node83
+docker restart saseul-node84
+docker restart saseul-node85
+docker restart saseul-node86
+docker restart saseul-node88
+docker restart saseul-node90
