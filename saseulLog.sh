50de5f55629ab39e635ab6d24d0826e9fa5ea94c
ChoRongChoRong <46623282+ChoRongChoRong-Saseul@users.noreply.github.com>
2023-10-18 21:59:18 +0900

    Create saseulLog.sh

---
+++ saseulLog.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NODE_NO=19
+
+NODES=$(seq 80 $((80 + $NODE_NO + 1)))
+
+for i in $NODES
+do
+  if [[ $i == "87" ]]; then
+    continue
+  fi
+  if [[ $i == "95" ]]; then
+    continue
+  fi
+
+
+#while [ $number -le $END ]
+#do
+  A=$(docker exec -i saseul-node$i saseul-script log )
+  echo "**********"
+  echo "Node $i Port: ""$A"
+  echo "**********"
+  sleep 1
+  ((number++))
+done
+
+echo "*******************************************************************"
+echo "${foods[@]}"
+echo "*******************************************************************"
