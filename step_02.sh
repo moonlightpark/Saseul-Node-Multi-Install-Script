bb76b97cdb842e041d9b2226998db7563797db40
ChoRongChoRong <46623282+ChoRongChoRong-Saseul@users.noreply.github.com>
2023-10-19 17:35:51 +0900

    Create step_02.sh

---
+++ step_02.sh
@@ -0,0 +1,83 @@
+#!/bin/bash --login
+
+docker exec -i saseul-node80 saseul-install
+docker exec -i saseul-node81 saseul-install
+docker exec -i saseul-node82 saseul-install
+docker exec -i saseul-node83 saseul-install
+docker exec -i saseul-node84 saseul-install
+docker exec -i saseul-node85 saseul-install
+docker exec -i saseul-node86 saseul-install
+docker exec -i saseul-node88 saseul-install
+docker exec -i saseul-node89 saseul-install
+docker exec -i saseul-node90 saseul-install
+docker exec -i saseul-node91 saseul-install
+docker exec -i saseul-node92 saseul-install
+docker exec -i saseul-node93 saseul-install
+docker exec -i saseul-node94 saseul-install
+docker exec -i saseul-node96 saseul-install
+docker exec -i saseul-node97 saseul-install
+docker exec -i saseul-node98 saseul-install
+docker exec -i saseul-node99 saseul-install
+docker exec -i saseul-node100 saseul-install
+
+
+docker exec -i saseul-node80 saseul-script setenv
+docker exec -i saseul-node81 saseul-script setenv
+docker exec -i saseul-node82 saseul-script setenv
+docker exec -i saseul-node83 saseul-script setenv
+docker exec -i saseul-node84 saseul-script setenv
+docker exec -i saseul-node85 saseul-script setenv
+docker exec -i saseul-node86 saseul-script setenv
+docker exec -i saseul-node88 saseul-script setenv
+docker exec -i saseul-node89 saseul-script setenv
+docker exec -i saseul-node90 saseul-script setenv
+docker exec -i saseul-node91 saseul-script setenv
+docker exec -i saseul-node92 saseul-script setenv
+docker exec -i saseul-node93 saseul-script setenv
+docker exec -i saseul-node94 saseul-script setenv
+docker exec -i saseul-node96 saseul-script setenv
+docker exec -i saseul-node97 saseul-script setenv
+docker exec -i saseul-node98 saseul-script setenv
+docker exec -i saseul-node99 saseul-script setenv
+docker exec -i saseul-node100 saseul-script setenv
+
+docker exec -i saseul-node80 saseul-script setenv --endpoint 0.0.0.0:80
+docker exec -i saseul-node81 saseul-script setenv --endpoint 0.0.0.0:81
+docker exec -i saseul-node82 saseul-script setenv --endpoint 0.0.0.0:82
+docker exec -i saseul-node83 saseul-script setenv --endpoint 0.0.0.0:83
+docker exec -i saseul-node84 saseul-script setenv --endpoint 0.0.0.0:84
+docker exec -i saseul-node85 saseul-script setenv --endpoint 0.0.0.0:85
+docker exec -i saseul-node86 saseul-script setenv --endpoint 0.0.0.0:86
+docker exec -i saseul-node88 saseul-script setenv --endpoint 0.0.0.0:88
+docker exec -i saseul-node89 saseul-script setenv --endpoint 0.0.0.0:89
+docker exec -i saseul-node90 saseul-script setenv --endpoint 0.0.0.0:90
+docker exec -i saseul-node91 saseul-script setenv --endpoint 0.0.0.0:91
+docker exec -i saseul-node92 saseul-script setenv --endpoint 0.0.0.0:92
+docker exec -i saseul-node93 saseul-script setenv --endpoint 0.0.0.0:93
+docker exec -i saseul-node94 saseul-script setenv --endpoint 0.0.0.0:94
+docker exec -i saseul-node96 saseul-script setenv --endpoint 0.0.0.0:96
+docker exec -i saseul-node97 saseul-script setenv --endpoint 0.0.0.0:97
+docker exec -i saseul-node98 saseul-script setenv --endpoint 0.0.0.0:98
+docker exec -i saseul-node99 saseul-script setenv --endpoint 0.0.0.0:99
+docker exec -i saseul-node100 saseul-script setenv --endpoint 0.0.0.0:100
+
+
+docker exec -i saseul-node80 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node81 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node82 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node83 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node84 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node85 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node86 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node88 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node89 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node90 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node91 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node92 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node93 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node94 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node96 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node97 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node98 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node99 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
+docker exec -i saseul-node100 saseul-script setenv -m xxxxxxxxxxxxxxxxxx
